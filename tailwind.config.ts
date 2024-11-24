ee478491814cf1c6e5205c5f617cbec2b86c1f6d
s0m3thln9 <dunlock67@gmail.com>
2024-11-24 21:19:03 +0300

    next reinit

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
