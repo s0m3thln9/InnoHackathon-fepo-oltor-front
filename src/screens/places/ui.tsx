24e932681f609e701228bd39ed336e058e57b334
s0m3thln9 <dunlock67@gmail.com>
2024-11-27 22:50:01 +0300

    add some features to map

--- src/screens/places/ui.tsx
+++ src/screens/places/ui.tsx
@@ -5,9 +5,16 @@ import { useAppSelector } from '@/app/stores'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import { Map } from '@/features/places'
-import { useJsApiLoader } from '@react-google-maps/api'
+import { Libraries, useJsApiLoader } from '@react-google-maps/api'
 
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+
+const markers = [
+  { lat: 52.4355, lng: 30.9554, title: 'Marker 1' },
+  { lat: 52.4395, lng: 30.9954, title: 'Marker 2' },
+]
+
+const libs: Libraries = ['places']
 
 export const PlacesPage = () => {
   // const user = useAppSelector((state) => state.user.user)
@@ -20,14 +27,22 @@ export const PlacesPage = () => {
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: API_KEY,
-    libraries: ['places'],
+    googleMapsApiKey: API_KEY ? API_KEY : '',
+    libraries: libs,
   })
 
   return (
     <>
       <Header selectedItem='places' />
-      <main className='h-full'>{isLoaded ? <Map /> : <div>Loading</div>}</main>
+      <main className='h-full'>
+        {isLoaded ? (
+          <Map markers={markers} />
+        ) : (
+          <div className='h-full w-full flex items-center justify-center'>
+            Loading
+          </div>
+        )}
+      </main>
     </>
   )
 }
