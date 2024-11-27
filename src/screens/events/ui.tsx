3992da1188c14496eda543aedb6526d07589fe77
s0m3thln9 <dunlock67@gmail.com>
2024-11-27 12:04:46 +0300

    create events-page + slider

--- src/screens/events/ui.tsx
+++ src/screens/events/ui.tsx
@@ -8,11 +8,11 @@ export const EventsPage = () => {
     <div className='h-[100svh] flex flex-col'>
       <Header />
       <main className='flex flex-col items-center justify-center bg-gradient-to-b from-background-main-linear-first to-background-main-linear-second'>
-        <h1 className='text-[96px] text-text mt-10'>events</h1>
+        <h1 className='text-[96px] text-text mt-8'>events</h1>
         <p className='text-2xl text-text-secondary mt-5'>
           First, select the upcoming event
         </p>
-        <Slider />
+        <Slider className='mt-24' />
       </main>
       <footer className='bg-background-main-linear-second h-full w-full'></footer>
     </div>
--- src/widgets/home/slider/ui.tsx
+++ src/widgets/home/slider/ui.tsx
@@ -1,75 +1,47 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
-import Image from 'next/image'
+import 'swiper/css/free-mode'
+import Image, { StaticImageData } from 'next/image'
 import image1 from '../../../../public/images/wedding_foto.png'
 import image2 from '../../../../public/images/bd_cake.png'
 import image3 from '../../../../public/images/corporativ_foto.png'
 import image4 from '../../../../public/images/ch_foto.png'
+import image5 from '../../../../public/images/bs_foto.png'
+import image6 from '../../../../public/images/other_foto.png'
+import { cn } from '@/shared/libs'
 
-export const Slider = () => {
+interface SliderProps {
+  className?: string
+}
+
+const slides: StaticImageData[] = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+]
+
+export const Slider: FC<SliderProps> = ({ className }) => {
   return (
     <Swiper
-      spaceBetween={50}
       slidesPerView={4}
-      className='w-full'
+      freeMode={true}
+      className={cn('w-full select-none', className)}
     >
-      <SwiperSlide>
-        <Image
-          src={image1}
-          alt=''
-          width={300}
-          height={300}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image2}
-          alt=''
-          width={300}
-          height={300}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image3}
-          alt=''
-          width={300}
-          height={300}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image4}
-          alt=''
-          width={300}
-          height={300}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image4}
-          alt=''
-          width={300}
-          height={300}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image4}
-          alt=''
-          width={300}
-          height={300}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image
-          src={image4}
-          alt=''
-          width={300}
-          height={300}
-        />
-      </SwiperSlide>
+      {slides.map((slide) => (
+        <SwiperSlide key={slide.src}>
+          <Image
+            src={slide.src}
+            alt={slide.src}
+            width={slide.width}
+            height={slide.height}
+            className='justify-self-center opacity-70'
+          />
+        </SwiperSlide>
+      ))}
     </Swiper>
   )
 }
